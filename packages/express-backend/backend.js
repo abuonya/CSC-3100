fb119df15fde2da1082753ce57ca5c74bc168498
Annalise Buonya <abuonya@calpoly.edu>
2026-09-09 09:23:02 -0700

    My first REST API (Node.js/Express)

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,78 @@
+// backend.js
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Meow!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+});
